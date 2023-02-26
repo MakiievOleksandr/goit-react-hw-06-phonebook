8d1a701fc6642346808d81bd257e2ca6fb4efacb
Makiiev Oleksandr <alsermak@gmail.com>
2023-02-26 20:48:22 +0200

    add redux slice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
-export const App = () => {
-  return <div></div>;
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+
+import Contacts from './Contacts/Contacts';
+
+import { store, persistor } from 'redux/store';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Contacts />
+      </PersistGate>
+    </Provider>
+  );
 };
+
+export default App;
